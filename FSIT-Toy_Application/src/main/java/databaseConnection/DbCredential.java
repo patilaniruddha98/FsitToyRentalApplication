477ad6b0e57f0dc8e2603fa2c151d92a3ba02787
Aniruddha patil <patilaniruddha98@gmail.com>
2021-05-29 22:38:21 +0530

    commit again

--- FSIT-Toy_Application/src/main/java/databaseConnection/DbCredential.java
+++ FSIT-Toy_Application/src/main/java/databaseConnection/DbCredential.java
@@ -2,9 +2,9 @@ package databaseConnection;
 
 public class DbCredential {
 
-	static String driverName = "com.mysql.jdbc.Driver";
+	static String driverName = "com.mysql.cj.jdbc.Driver";
 	static String url = "jdbc:mysql://127.0.0.1:3306/Toy_Application";
 	static String user = "root";
-	static String password = "harshwardhan";
+	static String password = "admin";
 
 }
